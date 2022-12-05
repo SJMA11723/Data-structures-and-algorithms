8b8a07668874a80e49a34fe1d6c9d47058ea78a4
Jorge Raúl Tzab López <tzablopezjorgeraul@gmail.com>
2022-12-04 23:27:14 -0600

    Implementación de segment tree acortdada

--- Segment Tree/segmentTree.cpp
+++ Segment Tree/segmentTree.cpp
@@ -7,66 +7,41 @@
 
 using namespace std;
 
-struct nodo{
-    int left, right;
-    int val;
-};
-
 struct SegmentTree{
-    int n, NEUTRO;
-    int *arr;
-    nodo *nodos;
-    int (*f)(const int &a, const int &b);
-
-    SegmentTree(int n_size, int nums[], int (*func)(const int &a, const int &b), int neutro){
-        n = n_size;
-        arr = new int[n + 1];
-        memcpy(arr, nums, sizeof(int) * (n + 1));
-        nodos = new nodo[4*n + 1];
-        f = func;
-        NEUTRO = neutro;
-        build(1, n);
-    }
-
-    void build(int l, int r, int pos = 1){
-        nodos[pos].left = l;
-        nodos[pos].right = r;
-
-        if(nodos[pos].left == nodos[pos].right){
-            nodos[pos].val = arr[r];
-            return;
+    struct nodo{
+        int val;
+        nodo():val(0){}/// inicializa con el neutro
+        nodo(int x):val(x){}
+        const nodo operator+(const nodo &b)const{
+            return nodo(val + b.val);
         }
-
-        int mitad = (nodos[pos].left + nodos[pos].right) / 2;
-
-        build(l, mitad, pos * 2);
-        build(mitad + 1, r, pos * 2 + 1);
-
-        nodos[pos].val = f(nodos[pos * 2].val, nodos[pos * 2 + 1].val);
+    }*nodos;
+    int *arr;
+    SegmentTree(int n){
+        arr = new int[n + 1];
+        nodos = new nodo[4*n + 1];
     }
 
-    void update(int x, int idx, int pos = 1){
-        if(idx < nodos[pos].left || nodos[pos].right < idx) return;
-
-        if(nodos[pos].left == nodos[pos].right){
+    void update(int x, int idx, int left, int right, int pos = 1){
+        if(idx < left || right < idx) return;
+        if(left == right){
             arr[idx] += x;
             nodos[pos].val += x;
             return;
         }
 
-        update(x, idx, pos * 2);
-        update(x, idx, pos * 2 + 1);
+        int mitad = (left + right) / 2;
+        update(x, idx, left, mitad, pos * 2);
+        update(x, idx, mitad + 1, right, pos * 2 + 1);
 
-        nodos[pos].val = f(nodos[pos * 2].val, nodos[pos * 2 + 1].val);
+        nodos[pos] = nodos[pos * 2] + nodos[pos * 2 + 1];
     }
 
-    int query(int left, int right, int pos = 1){
-        if(right < nodos[pos].left || nodos[pos].right < left) return NEUTRO;
-
-        if(left <= nodos[pos].left && nodos[pos].right <= right)
-            return nodos[pos].val;
-
-        return f(query(left, right, pos * 2), query(left, right, pos * 2 + 1));
+    nodo query(int l, int r, int left, int right, int pos = 1){
+        if(r < left || right < l) return 0; /// Devuelve el neutro
+        if(l <= left && right <= r) return nodos[pos].val;
+        int mitad = (left + right) / 2;
+        return query(l, r, left, mitad, pos * 2) + query(l, r, mitad + 1, right, pos * 2 + 1);
     }
 
     ~SegmentTree(){
@@ -75,26 +50,25 @@ struct SegmentTree{
     }
 };
 
-int suma(const int &a, const int &b){return a + b;}
-
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     int n, q;
     cin >> n >> q;
-    int arr[n + 1];
-    for(int i = 1; i <= n; ++i) cin >> arr[i];
-
-    SegmentTree seg(n, arr, suma, 0);
+    SegmentTree seg(n);
+    for(int i = 1; i <= n; ++i){
+        int x; cin >> x;
+        seg.update(x, i, 1, n);
+    }
 
     while(q--){
         int a, b;
         char c;
         cin >> c >> a >> b;
         if(c == 'u'){
-            seg.update(b, a);
-        } else cout << seg.query(a, b) << '\n';
+            seg.update(b, a, 1, n);
+        } else cout << seg.query(a, b, 1, n).val << '\n';
     }
 
 }
--- Segment Tree/segmentTreeLazy.cpp
+++ Segment Tree/segmentTreeLazy.cpp
@@ -7,97 +7,61 @@
 
 using namespace std;
 
-struct nodo{
-    int left, right;
-    int val;
-};
-
 struct SegmentTree{
-    int n, NEUTRO;
-    int *arr;
-    nodo *nodos, *lazy;
-    int (*f)(const int &a, const int &b);
-
-    SegmentTree(int n_size, int nums[], int (*func)(const int &a, const int &b), int neutro){
-        n = n_size;
-        arr = new int[n + 1];
-        memcpy(arr, nums, sizeof(int) * (n + 1));
-        nodos = new nodo[4*n + 1];
-        lazy = new nodo[4*n + 1];
-        f = func;
-        NEUTRO = neutro;
-        build(1, n);
-    }
-
-    void build(int l, int r, int pos = 1){
-        lazy[pos].val = 0;
-
-        nodos[pos].left = l;
-        nodos[pos].right = r;
-
-        if(nodos[pos].left == nodos[pos].right){
-            nodos[pos].val = arr[r];
-            return;
+    struct nodo{
+        int val, lazy;
+        nodo():val(0), lazy(0){}/// inicializa con el neutro y sin lazy pendiente
+        nodo(int x):val(x){}
+        const nodo operator+(const nodo &b)const{
+            return nodo(val + b.val);
         }
-
-        int mitad = (nodos[pos].left + nodos[pos].right) / 2;
-
-        build(l, mitad, pos * 2);
-        build(mitad + 1, r, pos * 2 + 1);
-
-        nodos[pos].val = f(nodos[pos * 2].val, nodos[pos * 2 + 1].val);
+    }*nodos;
+    int *arr;
+    SegmentTree(int n){
+        arr = new int[n + 1];
+        nodos = new nodo[4 * n + 1];
     }
 
-    void pushLazy(int pos){
-        int left = nodos[pos].left,
-            right = nodos[pos].right,
-            tam = abs(right - left + 1);
-
+    void pushLazy(int pos, int left, int right){
+        int tam = abs(right - left + 1);
         if(1 < tam){
-            lazy[pos * 2].val += lazy[pos].val;
-            lazy[pos * 2 + 1].val += lazy[pos].val;
+            nodos[pos * 2].lazy += nodos[pos].lazy;
+            nodos[pos * 2 + 1].lazy += nodos[pos].lazy;
         }
         /// IMPORTANTE: ESTE UPDATE ES PARA SUMA/RESTA. CAMBIAR SI ES NECESARIO
-        nodos[pos].val += lazy[pos].val * tam;
-        lazy[pos].val = 0;
+        nodos[pos].val += nodos[pos].lazy * tam;
+        nodos[pos].lazy = 0;
     }
 
-    void update(int x, int left, int right, int pos = 1){
-        pushLazy(pos);
-
-        if(right < nodos[pos].left || nodos[pos].right < left) return;
-
-        if(left <= nodos[pos].left && nodos[pos].right <= right){
-            lazy[pos].val += x;
-            pushLazy(pos);
+    void update(int x, int l, int r, int left, int right, int pos = 1){
+        pushLazy(pos, left, right);
+        if(r < left || right < l) return;
+        if(l <= left && right <= r){
+            nodos[pos].lazy += x;
+            pushLazy(pos, left, right);
             return;
         }
 
-        update(x, left, right, pos * 2);
-        update(x, left, right, pos * 2 + 1);
+        int mitad = (left + right) / 2;
+        update(x, l, r, left, mitad, pos * 2);
+        update(x, l, r, mitad + 1, right, pos * 2 + 1);
 
-        nodos[pos].val = f(nodos[pos * 2].val, nodos[pos * 2 + 1].val);
+        nodos[pos] = nodos[pos * 2] + nodos[pos * 2 + 1];
     }
 
-    int query(int left, int right, int pos = 1){
-        pushLazy(pos);
-
-        if(right < nodos[pos].left || nodos[pos].right < left) return NEUTRO;
-
-        if(left <= nodos[pos].left && nodos[pos].right <= right)
-            return nodos[pos].val;
-
-        return f(query(left, right, pos * 2), query(left, right, pos * 2 + 1));
+    nodo query(int l, int r, int left, int right, int pos = 1){
+        pushLazy(pos, left, right);
+        if(r < left || right < l) return 0; /// Devuelve el neutro
+        if(l <= left && right <= r) return nodos[pos].val;
+        int mitad = (left + right) / 2;
+        return query(l, r, left, mitad, pos * 2) + query(l, r, mitad + 1, right, pos * 2 + 1);
     }
 
     ~SegmentTree(){
         delete[] nodos;
-        delete[] lazy;
         delete[] arr;
     }
 };
-
-int suma(const int &a, const int &b){return a + b;}
 
 int main(){
     ios_base::sync_with_stdio(0);
@@ -105,10 +69,11 @@ int main(){
     cout.tie(0);
     int n, q;
     cin >> n >> q;
-    int arr[n + 1];
-    for(int i = 1; i <= n; ++i)
-        cin >> arr[i];
-    SegmentTree seg(n, arr, suma, 0);
+    SegmentTree seg(n);
+    for(int i = 1; i <= n; ++i){
+        int x; cin >> x;
+        seg.update(x, i, i, 1, n);
+    }
 
     while(q--){
         int a, b;
@@ -117,8 +82,8 @@ int main(){
         cin >> c >> a >> b;
         if(c == 'u'){
             cin >> x;
-            seg.update(x, a, b);
-        } else cout << seg.query(a, b) << '\n';
+            seg.update(x, a, b, 1, n);
+        } else cout << seg.query(a, b, 1, n).val << '\n';
     }
 
 }

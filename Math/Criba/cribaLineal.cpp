9b712587fd136e6fd085a5eb1bb2af2a8c839e8a
Jorge Raúl Tzab López <tzablopezjorgeraul@gmail.com>
2023-07-30 00:36:46 -0600

    Faltaba un punto y coma

--- Math/Criba/cribaLineal.cpp
+++ Math/Criba/cribaLineal.cpp
@@ -19,7 +19,7 @@ void cribaLineal(int n, vector<int> &primos){
     primos.clear();
     if(n < 2) return;
 
-    vector<int> lp(n + 1) /// lp[i] guarda el menor primo que divide a i
+    vector<int> lp(n + 1); /// lp[i] guarda el menor primo que divide a i
 
     for(long long i = 2; i <= n; ++i){
         if(!lp[i]){ /// si lp[i] = 0, entonces i es primo

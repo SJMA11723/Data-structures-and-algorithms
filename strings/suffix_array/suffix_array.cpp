ea97c7ad85fc14d939dd99a6c2a00a1ba65f7544
Jorge Raúl Tzab López <tzablopezjorgeraul@gmail.com>
2023-10-23 21:16:39 -0600

    Estructuras de datos para strings

---
+++ strings/suffix_array/suffix_array.cpp
@@ -0,0 +1,92 @@
+/**
+* Author: Jorge Raul Tzab Lopez
+* Github: https://github.com/SJMA11723
+*/
+
+#include <bits/stdc++.h>
+#define MAXN 10001
+
+using namespace std;
+
+int SA[MAXN], mrank[MAXN];
+int tmpSA[MAXN], tmpMrank[MAXN];
+void countingSort(int k, int n){
+    int freqs[MAXN] = {};
+    for(int i = 0; i < n; ++i){
+        if(i + k < n) freqs[ mrank[i + k] ]++;
+        else freqs[0]++;
+    }
+    int m = max(100, n);
+    for(int i = 0, sfs = 0; i < m; ++i){
+        int f = freqs[i];
+        freqs[i] = sfs;
+        sfs += f;
+    }
+    for(int i = 0; i < n; ++i){
+        if(SA[i] + k < n) tmpSA[ freqs[mrank[ SA[i] + k ]]++ ] = SA[i];
+        else tmpSA[ freqs[0]++ ] = SA[i];
+    }
+    for(int i = 0; i < n; ++i) SA[i] = tmpSA[i];
+}
+
+void buildSA(string &str){
+    int n = str.size();
+    for(int i = 0; i < n; ++i){
+        mrank[i] = str[i] - '#';
+        SA[i] = i;
+    }
+    for(int k = 1; k < n; k <<= 1){
+        countingSort(k, n);
+        countingSort(0, n);
+        int r = 0;
+        tmpMrank[ SA[0] ] = 0;
+        for(int i = 1; i < n; ++i){
+            if(mrank[ SA[i] ] != mrank[ SA[i - 1] ] || mrank[ SA[i] + k ] != mrank[ SA[i - 1] + k ])
+                tmpMrank[ SA[i] ] = ++r;
+            else
+                tmpMrank[ SA[i] ] = r;
+        }
+        for(int i = 0; i < n; ++i) mrank[i] = tmpMrank[i];
+    }
+}
+
+inline bool suff_compare1(int idx,const string &pattern) {
+    return (s.substr(idx).compare(0, pattern.size(), pattern) < 0);
+}
+
+inline bool suff_compare2(const string &pattern,int idx) {
+    return (s.substr(idx).compare(0, pattern.size(), pattern) > 0);
+}
+pair<int,int> match(const string &pattern) {
+    int *low = lower_bound (SA, SA + s.size(), pattern, suff_compare1);
+    int *up = upper_bound (SA, SA + s.size(), pattern, suff_compare2);
+    return make_pair((int)(low - SA),(int)(up - SA));
+}
+
+
+int lcp[MAXN];
+void buildLCP(string &str){
+    int n = str.size();
+    int phi[n];
+    phi[SA[0]] = -1;
+    for(int i = 1; i < n; ++i) phi[ SA[i] ] = SA[i - 1];
+    int plcp[n];
+    int k = 0;
+    for(int i = 0; i < n; ++i){
+        if(phi[i] == -1){
+            plcp[i] = 0;
+            continue;
+        }
+        while(i + k < n && phi[i] + k < n && str[i + k] == str[phi[i] + k]) k++;
+        plcp[i] = k;
+        k = max(k - 1, 0);
+    }
+    for(int i = 0; i < n; ++i) lcp[i] = plcp[SA[i]];
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+}

5ddd982e4b960e9d739e6677c6e78cda3d44fe10
Jorge Raúl Tzab López <tzablopezjorgeraul@gmail.com>
2024-03-26 14:42:19 -0600

    Update lca.cpp
    Add macros and rename

--- graph/trees/lca/lca.cpp
+++ graph/trees/lca/lca.cpp
@@ -4,6 +4,9 @@
 */
 
 #include <bits/stdc++.h>
+#define LOGN 16
+#define MAXN 50001
+#define is_on(n, i) ((n) & (1 << (i)))
 
 using namespace std;
 
@@ -11,21 +14,21 @@ int depth[MAXN];
 int P[LOGN + 1][MAXN];
 vector<int> tree[MAXN];
 
-void precalc(int node, int parent, int d = 1){
+void precalc(int node, int p = 0, int d = 1){
     depth[node] = d;
-    P[0][node] = parent;
+    P[0][node] = p;
     for(int k = 1; k <= LOGN; ++k)
         P[k][node] = P[k - 1][P[k - 1][node]];
 
     for(int child : tree[node])
-        if(parent != child) precalc(child, node, d + 1);
+        if(p != child) precalc(child, node, d + 1);
 }
 
 int LCA(int a, int b){
     if(depth[b] < depth[a]) swap(a, b);
     int dif = depth[b] - depth[a];
     for(int k = LOGN; 0 <= k; --k)
-        if(isOn(dif, k)) b = P[k][b];
+        if(is_on(dif, k)) b = P[k][b];
 
     if(a == b) return a;
 
@@ -40,7 +43,7 @@ int LCA(int a, int b){
 
 int nth_ancestor(int u, int n){
     for(int k = LOGN; 0 <= k; --k)
-        if(isOn(n, k)) u = P[k][u];
+        if(is_on(n, k)) u = P[k][u];
     return u;
 }
 

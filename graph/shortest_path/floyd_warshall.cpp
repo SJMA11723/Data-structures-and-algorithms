423b55bdb244da1849b37eefc1feee7bfa7cfde0
Jorge Raúl Tzab López <tzablopezjorgeraul@gmail.com>
2024-05-10 01:12:07 -0600

    ultimo momento

---
+++ graph/shortest_path/floyd_warshall.cpp
@@ -0,0 +1,31 @@
+/**
+* Author: Jorge Raul Tzab Lopez
+* Github: https://github.com/SJMA11723
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<vector<int>> floyd_warshall(int n){
+    vector<vector<int>> d(n, vector<int>(n, INT_MAX));
+    /// aqui inicializa con la lista/matriz de adyacencia
+    /// luego calcula la dp
+    for(int k = 0; k < n; ++k){
+        for(int i = 0; i < n; ++i){
+            for(int j = 0; j < n; ++j){
+                if(d[i][k] == INT_MAX) continue;
+                if(d[k][i] == INT_MAX) continue;
+                if(d[i][j] > d[i][k] + d[k][j]) d[i][j] = d[i][k] + d[k][j];
+            }
+        }
+    }
+    return d;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+}

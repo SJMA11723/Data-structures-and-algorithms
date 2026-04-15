c97cacc4e8b40faaefb6ccc79b5aedb57ba1e450
Jorge Raúl Tzab López <tzablopezjorgeraul@gmail.com>
2026-04-15 10:45:26 -0600

    Kosajaru fixed (RTE)
    Create template.h and fix kosajaru. Reference updated

--- graph/scc/kosajaru.cpp
+++ graph/scc/kosajaru.cpp
@@ -1,54 +1,46 @@
-/**
-* Author: Jorge Raul Tzab Lopez
-* Github: https://github.com/SJMA11723
-*/
+#include "../../template.h"
 
-#include <bits/stdc++.h>
-#define MAXN 100001
+void dfs(int u, vi graph[], bool vis[], vi &topo_ord){
+    if(vis[u]) return;
+    vis[u] = true;
 
-using namespace std;
-
-void dfs(int node, vector<int> graph[], bool vis[], vector<int> &topo_ord){
-    if(vis[node]) return;
-    vis[node] = true;
-
-    for(int v : graph[node]) dfs(v, graph, vis, topo_ord);
-    topo_ord.push_back(node);
+    for(int v : graph[u]) dfs(v, graph, vis, topo_ord);
+    topo_ord.pb(u);
 }
 
-void assign_scc(int node, vector<int> inv_graph[], bool vis[], vector<int> &scc, const int id){
-    if(vis[node]) return;
-    vis[node] = true;
-    scc[node] = id;
-    for(int v : inv_graph[node]) assign_scc(v, inv_graph, vis, scc, id);
+void assign_scc(int u, vi inv_graph[], bool vis[], vi &scc, const int id){
+    if(vis[u]) return;
+    vis[u] = true;
+    scc[u] = id;
+    for(int v : inv_graph[u]) assign_scc(v, inv_graph, vis, scc, id);
 }
 
-pair<int, vector<int>> kosajaru(int n, vector<int> graph[], vector<int> inv_graph[]){
+pair<int, vi> kosajaru(int n, vi graph[], vi inv_graph[]){
     bool vis[n] = {};
-    vector<int> topo_ord;
+    vi topo_ord;
     for(int i = 0; i < n; ++i) dfs(i, graph, vis, topo_ord);
-    reverse(topo_ord.begin(), topo_ord.end());
+    reverse(all(topo_ord));
     memset(vis, 0, sizeof(vis));
-    vector<int> scc(n);
+    vi scc(n);
     int id = 0;
     for(int u : topo_ord) if(!vis[u]) assign_scc(u, inv_graph, vis, scc, id++);
     return {id, scc};
 }
 
-pair<vector<vector<int>>, vector<vector<int>>> build_scc_graph(int n, vector<int> graph[], int n_scc, const vector<int> &scc){
-    vector<vector<int>> scc_graph, inv_scc_graph;
+pair<vvi, vvi> build_scc_graph(int n, vi graph[], int max_scc, const vi &scc){
+    vvi scc_graph(max_scc), inv_scc_graph(max_scc);
     for(int u = 0; u < n; ++u)
         for(int v : graph[u])
             if(scc[u] != scc[v])
-                scc_graph[scc[u]].push_back(scc[v]);
+                scc_graph[scc[u]].pb(scc[v]);
 
-    for(int u = 0; u < n_scc; ++u){
-        sort(scc_graph[u].begin(), scc_graph[u].end());
-        auto it = unique(scc_graph[u].begin(), scc_graph[u].end());
+    for(int u = 0; u < max_scc; ++u){
+        sort(all(scc_graph[u]));
+        auto it = unique(all(scc_graph[u]));
         scc_graph[u].resize(it - scc_graph[u].begin());
 
         for(int v : scc_graph[u])
-            inv_scc_graph[v].push_back(u);
+            inv_scc_graph[v].pb(u);
     }
 
     return {scc_graph, inv_scc_graph};
@@ -59,19 +51,19 @@ int main(){
     cin.tie(0);
     cout.tie(0);
     int n, m; cin >> n >> m;
-    vector<int> graph[n], inv_graph[n];
+    vi graph[n], inv_graph[n];
     for(int i = 0; i < m; ++i){
         int a, b;
         cin >> a >> b;
         a--, b--;
-        graph[a].push_back(b);
-        inv_graph[b].push_back(a);
+        graph[a].pb(b);
+        inv_graph[b].pb(a);
     }
 
 
-    int n_scc;
-    vector<int> scc;
-    tie(n_scc, scc) = kosajaru(n, graph, inv_graph);
+    int max_scc;
+    vi scc;
+    tie(max_scc, scc) = kosajaru(n, graph, inv_graph);
 
     for(int i = 0; i < n;++i) cout << i << ": " << scc[i] << '\n';
 }
